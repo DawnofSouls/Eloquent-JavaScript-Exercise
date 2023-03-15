ca06207bb801946b7be14be123436d4863281568
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-03-15 11:58:47 -0700

    Created a looping triangle

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const triangleLoop = () => {
+    const length = 8;
+    let traingleResult = "";
+    for (let i = 0; i < length; i++){
+        traingleResult += "#";
+        console.log(traingleResult);
+    };
+};
